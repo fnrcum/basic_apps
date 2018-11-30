440dbc4fabd2f3ea280b8133bb45bccf15b7c45d
fnrcum <nfarcas@mail.com>
2018-11-30 09:44:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -162,6 +162,6 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     application.jinja_env.cache = {}
     # application.run(host="0.0.0.0", port=port)
-    # serve(application, listen='0.0.0.0:{}'.format(port))
-    application.run()
+    serve(application, listen='0.0.0.0:{}'.format(port))
+
 
